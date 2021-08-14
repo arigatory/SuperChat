c0f3f8ef45c08b6fe86162f70f39913ef485715d
Ivan MSI <arigatory@gmail.com>
2021-08-14 09:41:26 +0300

    done

--- SuperChat/MVVM/ViewModel/MainViewModel.cs
+++ SuperChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SuperChat.MVVM.Model;
+﻿using SuperChat.Core;
+using SuperChat.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,15 +9,54 @@ using System.Threading.Tasks;
 
 namespace SuperChat.MVVM.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        //commands
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+            });
 
             Messages.Add(new MessageModel
             {
